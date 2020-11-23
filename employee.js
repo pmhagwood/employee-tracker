013147f7c750621e9c5a102da9cf8cb78bb8018d
Paul Hagwood <SubtleKnife@Pauls-MacBook-Pro.local>
2020-11-23 16:31:01 -0500

    added more

--- employee.js
+++ employee.js
@@ -69,10 +69,10 @@ const viewEmployees = () => {
   connection.query(query, (err, res) => {
     if (err) throw err;
     console.log('\n');
-        console.log('VIEW ALL EMPLOYEES');
-        console.log('\n');
-        console.table(res);
-        addDepartment();
+    console.log('VIEW ALL EMPLOYEES');
+    console.log('\n');
+    console.table(res);
+    addDepartment();
   });
 };
 
@@ -87,9 +87,60 @@ const viewEmpbyDepartment = () => {
   connection.query(query, (err, res) => {
     if (err) throw err;
     console.log('\n');
-        console.log('VIEW EMPLOYEE BY DEPARTMENT');
-        console.log('\n');
-        console.table(res);
-        addDepartment();
+    console.log('VIEW EMPLOYEE BY MANAGER');
+    console.log('\n');
+    console.table(res);
+    addDepartment();
   });
 };
+
+const viewEmpbyManager = () => {
+  const query =
+    `SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
+    FROM employee
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
+    INNER JOIN department ON (department.id = role.department_id)
+    ORDER BY manager;`;
+  connection.query(query, (err, res) => {
+    if (err) throw err;
+    console.log('\n');
+    console.log('VIEW EMPLOYEE BY DEPARTMENT');
+    console.log('\n');
+    console.table(res);
+    addDepartment();
+  });
+};
+
+const addEmployee = () => {
+  inquirer
+    .prompt ([
+      {
+        name: 'role',
+        type
+      }
+    ])
+}
+
+const removeEmployee = () => {
+  inquirer
+    .prompt([
+      {
+        name: "remove",
+        type: "input",
+        message: "Enter the ID of the Employee you would like to remove: "
+      }
+    ])
+    .then(function (answer) {
+      connection.query('DELETE FROM employee WHERE ?',
+        {
+          id: answer.remove
+        },
+        function (err) {
+          if (err) throw err;
+          console.log('Employee has been removed');
+          addDepartment();
+        }
+      )
+    })
+}
